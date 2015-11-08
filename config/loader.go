dolohow/PhotoSaloon-backend

config: Add ability to read yaml config files

config/loader.go
@@ -1,0 +1,61 @@
+// Package config provides the ability to load yaml based config file
+// depending on environment variable "GO_ENV".
+package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	ErrMissingKey    = errors.New("No config filename for the given GO_ENV")
+	ErrReadFile      = errors.New("Could not load config file")
+	ErrCouldNotParse = errors.New("Could not parse config file")
+)
+
+// Filenames type is used to abstract underlying data type. The key
+// represents current value of GO_ENV environment variable and a value
+// filename in yaml format.
+type Filenames map[string]string
+
+// Config struct holds internal state of package.
+type Config struct {
+	env string
+}
+
+// NewConfigLoader returns a new config.
+func NewConfigLoader() *Config {
+	return &Config{env: getEnvVariable()}
+}
+
+// Load function unmarshales file from files map to v interface.
+func (c *Config) Load(v interface{}, files Filenames) error {
+	fileName, ok := files[c.env]
+
+	if !ok {
+		return ErrMissingKey
+	}
+
+	config, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return ErrReadFile
+	}
+
+	if err = yaml.Unmarshal(config, v); err != nil {
+		return ErrCouldNotParse
+	}
+
+	return nil
+}
+
+func getEnvVariable() string {
+	if env := os.Getenv("GO_ENV"); env != "" {
+		return env
+	}
+
+	return "development"
+}

config/loader_test.go
@@ -1,0 +1,92 @@
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+type testConfig struct {
+	TestField string `yaml:"test"`
+}
+
+func TestNewConfigLoader_Default(t *testing.T) {
+	config := NewConfigLoader()
+
+	if config.env != "development" {
+		t.Errorf("Expected env to equal 'development', got %q", config.env)
+	}
+}
+
+func TestNewConfigLoader_WithSetEnv(t *testing.T) {
+	os.Setenv("GO_ENV", "production")
+
+	config := NewConfigLoader()
+
+	if config.env != "production" {
+		t.Errorf("Expected env to equal 'production', got %q", config.env)
+	}
+}
+
+func TestLoader_OK(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "test.yaml"
+
+	mock := &testConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != nil {
+		t.Error("Expected err to equal null")
+	}
+
+	if mock.TestField != "example value" {
+		t.Errorf("Expected %q, got %q", "example value", mock.TestField)
+	}
+}
+
+func TestLoad_NoConfigFile(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "no_such_file.yaml"
+
+	mock := &testConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrReadFile {
+		t.Errorf("Expected err to equal %q, got %q", ErrReadFile, err)
+	}
+}
+
+func TestLoad_MissingKey(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["invalid_key"] = "test.yaml"
+
+	mock := &testConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrMissingKey {
+		t.Errorf("Expected err to equal %q, got %q", ErrMissingKey, err)
+	}
+}
+
+func TestLoad_InvalidConfigFile(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "invalid.yaml"
+
+	mock := &testConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrCouldNotParse {
+		t.Errorf("Expected err to equal %q, got %q", ErrCouldNotParse, err)
+	}
+}

Puksi-Team/RatioCare-backend

config/loader.go
@@ -1,0 +1,60 @@
+// Package config provides the ability to load yaml based config file
+// depending on environment variable "GO_ENV".
+package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	ErrMissingKey    = errors.New("No config filename for the given GO_ENV")
+	ErrNoSuchFile    = errors.New("Could not load config file")
+	ErrCouldNotParse = errors.New("Could not parse config file")
+)
+
+// Filenames type is used to abstract underlying data type. The key
+// represents current value of GO_ENV environment variable and a value
+// filename in yaml format.
+type Filenames map[string]string
+
+type config struct {
+	env string
+}
+
+// NewConfigLoader returns a new config.
+func NewConfigLoader() *config {
+	return &config{env: getEnvVariable()}
+}
+
+// Load function unmarshales file from files map to v interface.
+func (c *config) Load(v interface{}, files Filenames) error {
+	fileName, ok := files[c.env]
+
+	if !ok {
+		return ErrMissingKey
+	}
+
+	config, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return ErrNoSuchFile
+	}
+
+	if err = yaml.Unmarshal(config, v); err != nil {
+		return ErrCouldNotParse
+	}
+
+	return nil
+}
+
+func getEnvVariable() string {
+	if env := os.Getenv("GO_ENV"); env != "" {
+		return env
+	}
+
+	return "development"
+}

config/loader_test.go
@@ -1,0 +1,92 @@
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+type mockConfig struct {
+	TestField string `yaml:"test"`
+}
+
+func TestNewConfigLoader_Default(t *testing.T) {
+	config := NewConfigLoader()
+
+	if config.env != "development" {
+		t.Errorf("Expected env to equal 'development', got %q", config.env)
+	}
+}
+
+func TestNewConfigLoader_WithSetEnv(t *testing.T) {
+	os.Setenv("GO_ENV", "production")
+
+	config := NewConfigLoader()
+
+	if config.env != "production" {
+		t.Errorf("Expected env to equal 'production', got %q", config.env)
+	}
+}
+
+func TestLoader_OK(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "test.yaml"
+
+	mock := &mockConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != nil {
+		t.Error("Expected err to equal null")
+	}
+
+	if mock.TestField != "example value" {
+		t.Errorf("Expected %q, got %q", "example value", mock.TestField)
+	}
+}
+
+func TestLoad_NoConfigFile(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "no_such_file.yaml"
+
+	mock := &mockConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrNoSuchFile {
+		t.Errorf("Expected err to equal %q, got %q", ErrNoSuchFile, err)
+	}
+}
+
+func TestLoad_MissingKey(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["invalid_key"] = "test.yaml"
+
+	mock := &mockConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrMissingKey {
+		t.Errorf("Expected err to equal %q, got %q", ErrMissingKey, err)
+	}
+}
+
+func TestLoad_InvalidConfigFile(t *testing.T) {
+	os.Setenv("GO_ENV", "test")
+
+	filenames := make(map[string]string)
+	filenames["test"] = "invalid.yaml"
+
+	mock := &mockConfig{}
+
+	config := NewConfigLoader()
+
+	if err := config.Load(&mock, filenames); err != ErrCouldNotParse {
+		t.Errorf("Expected err to equal %q, got %q", ErrCouldNotParse, err)
+	}
+}